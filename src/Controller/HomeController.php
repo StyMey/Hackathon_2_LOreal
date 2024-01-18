1ff24b591d7569ca325d1abb12afe88c1b4bbf15
Stephdu33 <stephanievivien@hotmail.com>
2024-01-19 00:20:22 +0100

    modif score

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,21 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/', name: 'app_')]
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
-    }
+        $response = $this->client->request(
+            'GET',
+            'http://188.166.208.105:5000/chart/4/rides'
+        );
 
-    #[Route('/', name: 'chart')]
-    public function test(ChartBuilderInterface $chartBuilder): Response
-    {
-
+        $content = $response->getContent();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        return $this->render('home/index.html.twig', ['content' => $content]);
     }
 }
--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
 #[Route('/score', name: 'app_')]
 class ScoreController extends AbstractController
 {
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
     #[Route('/', name: 'score')]
     public function index(): Response
     {
-        return $this->render('score/score.html.twig');
+        $response = $this->client->request(
+            'GET',
+            'http://188.166.208.105:5000/chart/4/rides_profondes'
+        );
+
+        $content = $response->getContent();
+        return $this->render('score.html.twig', ['content' => $content]);
     }
 }
